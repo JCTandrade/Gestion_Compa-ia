3561ba35aa79aa6bd41b95dd14eef85b51c890f6
JCTamayo <tamayojuan754@gmail.com>
2024-08-24 11:31:08 -0500

    fix: se corrige autocrado de las tablas

--- src/main/java/com/JVNTecnologias/gestion_compania/entity/SucursalEntity.java
+++ src/main/java/com/JVNTecnologias/gestion_compania/entity/SucursalEntity.java
@@ -1,2 +1,42 @@
-package com.JVNTecnologias.gestion_compania.entity;public class SucursalEntity {
+package com.JVNTecnologias.gestion_compania.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "sucursal")
+public class SucursalEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_sucursal")
+    private Long idSucursal;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "responsable")
+    private String responsable;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "fecha_creacion")
+    private LocalDate createdAt;
+
+    @Column(name = "fecha_actualizacion")
+    private String updatedAt;
 }
